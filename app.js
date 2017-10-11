287012ff168b72028fe0277b912811701c14e993
bred <link.belal@gmail.com>
2017-10-11 13:50:03 +0530

    Default route to Login page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('rootpath')();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,16 +6,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-//Including config file
-var config = require('./config.json');
+var config = require('config.json');
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 app.set('view engine', 'html');
 
@@ -24,18 +20,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/release')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Defining Routes
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./controllers/login.controller'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
@@ -50,7 +46,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(3000, function (){
-	console.log("Server Up");
+	console.log("Server Up at port 3000");
 })
 
 module.exports = app;
---
+++ controllers/login.controller.js
@@ -0,0 +1,69 @@
+require('rootpath')();
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+
+console.log("Inside Controller..............");
+// return false;
+
+
+router.get('/', function (req, res) {
+	console.log('Inside get login controller');
+
+	// return false;
+    // log user out
+    //delete req.session.token;	
+	
+	// check if session is exists
+	// if(req.session.token){
+	// 	// redirect to returnUrl
+	// 	var returnUrl = req.query.returnUrl && decodeURIComponent(req.query.returnUrl) || '/dashboard';
+	// 	res.redirect(returnUrl);
+	// }
+	
+    // move success message into local variable so it only appears once (single read)
+    // var viewData = { success: req.session.success};
+    // delete req.session.success;
+
+    // console.log('final'); return false;
+    // router.use('auth', express.static('public/release'));
+
+    res.sendfile('angular/release/auth.html');
+    // res.redirect('auth', viewData);
+
+
+});
+
+router.post('/', function (req, res) {
+    console.log('Inside controller 1234567890');
+    return false;
+    // console.log(config.apiUrl_local);
+    // authenticate using api to maintain clean separation between layers    
+    request.post({
+        url: config.apiUrl_local + '/users/authenticate',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {		
+        if (error) {			
+            return res.render('login', { error: 'An error occurred !' });
+        }
+        // console.log("API RESPONSE:");
+        // console.log(response);
+        console.log('After api and service');
+        console.log(body);
+
+        if (!body.token) {			
+            return res.render('login', { error: body, username: req.body.username });
+        }
+
+        // save JWT token in the session to make it available to the angular app
+        req.session.token = body.token;
+		
+        // redirect to returnUrl
+        var returnUrl = req.query.returnUrl && decodeURIComponent(req.query.returnUrl) || '/dashboard';
+        res.redirect(returnUrl);
+    });
+});
+
+module.exports = router;
