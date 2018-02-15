2e50c5df9df80c61bedc4bae1870ff6dc63f824f
belal <link.belal@gmail.com>
2018-02-15 08:56:20 +0530

    smart tabble issue

--- public/admin/app/pages/customer/customerListInactiveCtrl.js
+++ public/admin/app/pages/customer/customerListInactiveCtrl.js
@@ -15,13 +15,15 @@
 
     // Defining parent data as object
     $scope.data = {};
+    $scope.rowCollection = [];
     // $scope.smartTableData = [];
     $scope.smartTablePageSize = 5;
-    var smartTableData_temp = new Array();
+   
     // Getting data from db
     $http.get("/admin/customer/getInActiveCustomer")
     .success(function (data, status, headers){
       $scope.data.customers = data;
+      var smartTableData_temp = new Array();
       angular.forEach($scope.data.customers, function(arrayItem, index){
         var temp_arr = {};
         temp_arr.id = parseInt(index) + 1;
@@ -33,17 +35,8 @@
         smartTableData_temp[index] = temp_arr;
           // $scope.smartTableData[index] = arrayItem;
       })
-
-      // $scope.smartTableData = data;
-      // $scope.smartTableData = smartTableData_temp;
-      // console.log($scope.smartTableData);
-      $timeout(function () {
-          //$scope.smartTableData = smartTableData_temp;
-
-      }, 1000);
-
-
-    })
+      $scope.rowCollection = smartTableData_temp;
+   })
     .error(function (data, status, header){
       console.log("Failed...........");
       //Handling 403 response
@@ -52,7 +45,7 @@
         $window.open(baseUrl,"_self");
       }
     });
-
+    $scope.smartTableData = [].concat($scope.rowCollection);
     $scope.changestatus = function(_id,status, customer_selector_id){
 
       var selector_id = customer_selector_id + 1;
@@ -92,68 +85,6 @@
       });
 
     }
-
-    $scope.smartTableData = [
-
-      {
-        "id": 50,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "rebekahgross@comtours.com",
-      },
-      {
-        "id": 51,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "earlinewoodward@comtours.com",
-      },
-      {
-        "id": 52,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "moranjohns@comtours.com",
-      },
-      {
-        "id": 53,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "nanettecooke@comtours.com",
-      },
-      {
-        "id": 54,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "daltonhendricks@comtours.com",
-      },
-      {
-        "id": 55,
-        "name": "Rebekah",
-        "mobile": 98798798798,
-        "email": "bennettpena@comtours.com",
-      },
-      // {
-      //   "id": 56,
-      //   "firstName": "Kellie",
-      //   "lastName": "Horton",
-      //   "username": "@Weiss",
-      //   "email": "kellieweiss@comtours.com",
-      // },
-      // {
-      //   "id": 57,
-      //   "firstName": "Hobbs",
-      //   "lastName": "Talley",
-      //   "username": "@Sanford",
-      //   "email": "hobbssanford@comtours.com",
-      // }
-    ];
-
-
-
-
-
-
-
-     // console.log($scope.smartTableData);
 
   }
 })();
